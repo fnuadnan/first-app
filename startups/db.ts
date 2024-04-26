7b315185290ce822dff76f75e50bdce6f0874273
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-04-26 16:36:43 -0500

    Setting up the db

--- startups/db.ts
+++ startups/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default function () {
   mongoose
-    .connect("mongodb://localhost:27017/playground")
+    .connect(process.env.DB_TEST)
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.log("Could not connect to MongoDB...", err));
 }
